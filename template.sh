69d8b92d33b8e6f948e6cbad9f2fadc213e3d51e
Chandrashekhar Buradkar <chandrashekharburadkar@Chandrashekhars-MacBook-Pro.local>
2025-09-14 17:02:40 +0530

    folder added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating directory
+
+mkdir -p src
+mkdir -p resource
+
+# creating files 
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch resource/trails.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directory and files created successfully!."
